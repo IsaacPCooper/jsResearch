6dbbef1c9f159651dfd6f1ae31d210c06106c050
Isaac Philip Cooper <Jordan.P.C.Austin@gmail.com>
2022-04-27 18:41:21 +0100

    added DOM variable interaction

---
+++ variables/Interacting with the DOM/index.js
@@ -0,0 +1,13 @@
+// Grab the h1 tag and store it as a variable
+var welcomeElement = document.getElementById("welcome-element");
+var myName = "Isaac";
+var greetingMessage = "Welcome to the site, ";
+
+var welcomingMessage = greetingMessage + myName;
+
+function welcomeMe(){
+    welcomeElement.innerText = welcomingMessage;
+}
+
+// Create two variables for greeting
+// Render the greeting on the html
