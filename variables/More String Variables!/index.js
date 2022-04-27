997d8adebb6c527ce0be6a8fb07ef1375bec23b1
Isaac Philip Cooper <Jordan.P.C.Austin@gmail.com>
2022-04-27 18:30:11 +0100

    more string variables!

---
+++ variables/More String Variables!/index.js
@@ -0,0 +1,8 @@
+var myFavouriteColor = "Black";
+var myName = "Isaac";
+
+var moreAboutMe = "My full name is:" + " " + myName + " " + "Cooper!";
+var aCoolFact = "My Favourite color is" + " " + myFavouriteColor + ".";
+
+console.log(moreAboutMe);
+console.log(aCoolFact);
