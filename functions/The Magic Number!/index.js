4c4ed9f84aef1262ece8bb2f077145868f3579de
Isaac Philip Cooper <Jordan.P.C.Austin@gmail.com>
2022-04-27 17:26:29 +0100

    added the magic number to functions folder

---
+++ functions/The Magic Number!/index.js
@@ -0,0 +1,9 @@
+// Set an initial value
+var startingNum = 20;
+
+// Make the value 42 with a function
+function theMagicNumber(){
+    var magic = startingNum + 22;
+
+    alert("The Magic Number is:" + " " + magic);
+}
