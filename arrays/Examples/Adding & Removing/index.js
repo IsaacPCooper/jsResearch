d442c25eebc576f89a74974c6519fd1526ab7d9f
Isaac Philip Cooper <Jordan.P.C.Austin@gmail.com>
2022-04-29 19:16:50 +0100

    push and pop

---
+++ arrays/Examples/Adding & Removing/index.js
@@ -0,0 +1,11 @@
+let bookQuotes = [
+    "What, you egg?",
+    "A Speck",
+]
+
+bookQuotes.push("I think, therefore I am.")
+console.log(bookQuotes);
+
+// Remove the last item
+bookQuotes.pop();
+console.log(bookQuotes);
