d8c0d6c4b47ceda2d3c41dab1a891932c1a3c19d
Isaac Philip Cooper <Jordan.P.C.Austin@gmail.com>
2022-04-29 19:12:48 +0100

    multiple data types array

---
+++ arrays/Examples/Multiple data types/index.js
@@ -0,0 +1,9 @@
+let myInfo = [
+    "Isaac Cooper",
+    23,
+    "Software Developer"
+]
+
+console.log("I am " + myInfo[0])
+console.log("I am " + myInfo[1] + " years old");
+console.log("My Job title is: " + myInfo[2]);
