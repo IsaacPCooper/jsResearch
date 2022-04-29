b109b03ac79cb1fd6b00809e8f8faff6e380d555
Isaac Philip Cooper <Jordan.P.C.Austin@gmail.com>
2022-04-29 18:26:41 +0100

    newcard function

--- Black Jack App/index.js
+++ Black Jack App/index.js
@@ -7,12 +7,14 @@ let isAlive = true;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
 
 
 function startGame(){
-
+    cardsEl.textContent= "Cards: " +  firstCard + " " + secondCard;
     sumEl.textContent= "Sum: " +  sum;
+    
 // if condition
 if (sum <= 20) {
     message = "Draw a card?"
@@ -28,5 +30,9 @@ if (sum <= 20) {
  messageEl.textContent = message;
  console.log(message);
 
+}
+
+function newCard(){
+    console.log("Drawing a New card from the deck....")
 }
 
