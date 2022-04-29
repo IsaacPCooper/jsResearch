97fe159e51b78ca8892f604fde8f2455a6b3235e
Isaac Philip Cooper <Jordan.P.C.Austin@gmail.com>
2022-04-29 18:20:06 +0100

    sum element js / html

--- Black Jack App/index.js
+++ Black Jack App/index.js
@@ -6,10 +6,13 @@ let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
 
 
 
 function startGame(){
+
+    sumEl.textContent= "Sum: " +  sum;
 // if condition
 if (sum <= 20) {
     message = "Draw a card?"
