a4e2718eb721872dd8c2ea382089b85aeff4aba4
Isaac Philip Cooper <Jordan.P.C.Austin@gmail.com>
2022-04-29 17:46:33 +0100

    robustness of BlackJack

--- Black Jack App/index.js
+++ Black Jack App/index.js
@@ -1,14 +1,16 @@
 // Variables for two cards
 let firstCard = 2;
 let secondCard = 4;
-
-// Sum of both cards
 let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
 
 // if condition
-if (sum < 21) {
-    // Draw a card
+if (sum <= 20) {
+    console.log("Draw a card?")
 } else if (sum === 21){
+    hasBlackJack = true;
     // YOU WIN!
 }
+console.log("It's over!")
     // You lose!
