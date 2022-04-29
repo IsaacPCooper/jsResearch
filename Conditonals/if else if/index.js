cdfe96f66998247e612c0398b7d1cf7a85135276
Isaac Philip Cooper <Jordan.P.C.Austin@gmail.com>
2022-04-29 17:00:20 +0100

    if else if

---
+++ Conditonals/if else if/index.js
@@ -0,0 +1,17 @@
+// Define variables
+let num = 10;
+
+
+// if number is less than 10, "less than 10"
+if (num < 10) {
+    console.log("The Number is less than 10");
+}
+//else if number is exactly 10 "number is 10!"
+ else if (num === 10){
+    console.log("The Number is 10");
+ }
+// else number is more than 10
+ else {
+    console.log("The Number is more than 10");
+ }
+
