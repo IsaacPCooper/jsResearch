d3428325131accbce08ac7ed98dea9a710855433
Isaac Philip Cooper <Jordan.P.C.Austin@gmail.com>
2022-04-29 19:29:48 +0100

    for loops with practical examples

---
+++ loops/for loops/counting/index.js
@@ -0,0 +1,14 @@
+// Count to 10
+for ( let count = 1; count < 11; count += 1 ) {
+console.log(count);
+}
+
+// count to 100 in 10s
+for (let i = 0; i < 100; i + 10){
+    console.log(i);
+}
+
+// Practical example - increment by 5
+for (let appleSlices = 5; appleSlices < 50; appleSlices += 5){
+    console.log("Apple Slices: " + appleSlices)
+}
