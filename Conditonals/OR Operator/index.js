10eb716029f5aa16c7d697a6ca29ef11f4407fbd
Isaac Philip Cooper <Jordan.P.C.Austin@gmail.com>
2022-05-03 17:33:49 +0100

    logical or operator

---
+++ Conditonals/OR Operator/index.js
@@ -0,0 +1,10 @@
+let hasDoneX = true;
+let hasDoneY = false
+
+if (hasDoneX === true || hasDoneY === true){
+    generatePraise();
+}
+
+function generatePraise(){
+    alert("Good Job, You have completed 1 task or more!")
+}
