ee6781afcb537bd08c763bec32dec60983a3c1b4
Isaac Philip Cooper <Jordan.P.C.Austin@gmail.com>
2022-05-03 17:47:30 +0100

    Pizza Time

---
+++ objects/intro to objects/index.js
@@ -0,0 +1,12 @@
+let pizza = {
+    base: "Thin",
+    sauce: "BBQ",
+    cheese: "Mozarella",
+    toppings: ["Chicken", "Bacon", "Mushrooms"],
+    size: "10 inches",
+    price: 9.99,
+    isTakeout: true
+}
+
+alert("You have ordered a pizza with a " + pizza.base + " base")
+alert("This Pizza costs £ " + pizza.price);
