9d99b30319d5d8271662710db1caf693608f35cf
Isaac Philip Cooper <Jordan.P.C.Austin@gmail.com>
2022-05-03 18:22:02 +0100

    unshift + shift

---
+++ arrays/shift + unshift/index.js
@@ -0,0 +1,7 @@
+let foodTypes = ["Carbs", "Fruits", "Veg"]
+
+console.log(foodTypes)
+foodTypes.shift() // remove the first item in the array
+console.log(foodTypes)
+foodTypes.unshift("Grains") // add to the start of the array
+console.log(foodTypes)
