d4a190db8d636c75f77792d3d77bbd4bd3869ba0
Isaac Philip Cooper <Jordan.P.C.Austin@gmail.com>
2022-05-03 17:31:20 +0100

    is alive and blackjack if check

--- Black Jack App/index.js
+++ Black Jack App/index.js
@@ -53,10 +53,12 @@ if (sum <= 20) {
 }
 
 function newCard(){
+    if (isAlive === true && hasBlackJack === false){
     let card = getRandomCard();
     sum += card;
     cards.push(card);
     renderGame();
+    }
 }
 
 
