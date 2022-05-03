7614cb2c2903f95d0c13e7128f5cdec3076951db
Isaac Philip Cooper <Jordan.P.C.Austin@gmail.com>
2022-05-03 17:07:51 +0100

    pulling through random number for cards

--- Black Jack App/index.js
+++ Black Jack App/index.js
@@ -11,7 +11,8 @@ let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
 function getRandomCard() {
-    return Math.floor(Math.random() * 6 ) + 1;
+    let randomCard = Math.floor(Math.random() * 13 ) + 1; // range between 1-13
+    return randomCard;
 }
 
 function startGame() {
