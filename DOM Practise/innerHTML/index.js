4ed71c559e1ed2068ac7b640c0c05e769ae16a99
Isaac Philip Cooper <Jordan.P.C.Austin@gmail.com>
2022-05-05 18:41:00 +0100

    function rendering html

--- DOM Practise/innerHTML/index.js
+++ DOM Practise/innerHTML/index.js
@@ -1,3 +1,8 @@
 const mainDiv = document.getElementById("main-div");
 
-mainDiv.innerHTML += "<button>" + "BUY NOW!" + "</button>"
+mainDiv.innerHTML += "<button onclick='buy()'>" + "BUY NOW!" + "</button>"
+
+
+function buy(){
+    mainDiv.innerHTML += "<p> Thanks for your purchase! </p>"
+}
