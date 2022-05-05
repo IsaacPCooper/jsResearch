cbfde6fdb17db39db41373b96df1be99af92f4f9
Isaac Philip Cooper <Jordan.P.C.Austin@gmail.com>
2022-05-05 18:54:58 +0100

    added new function to render on click

--- Chrome Extension/index.js
+++ Chrome Extension/index.js
@@ -9,17 +9,14 @@ let myLeads = ["example1","example2", "example3"];
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     console.log(myLeads)
+    renderLeads();
 })
 
+function renderLeads() {
 let listItems = "";
 for (let i = 0; i < myLeads.length; i++){
     listItems += "<li>" + myLeads[i] + "</li>";
-    //ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
-
-    // This also works, but may be harder to understand!
-    // document.createElement("li")
-    // li.textContent = myLeads[i];
-    // ulEl.append(li);
 }
 ulEl.innerHTML = listItems;
+}
 
