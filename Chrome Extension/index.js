52fa308bb620a7d06bffdd0cea45aa7ff2bdc48e
Isaac Philip Cooper <Jordan.P.C.Austin@gmail.com>
2022-05-05 18:24:57 +0100

    for loop logging out all items in array

--- Chrome Extension/index.js
+++ Chrome Extension/index.js
@@ -1,5 +1,5 @@
 const inputBtn = document.getElementById("input-btn");
-let myLeads = [];
+let myLeads = ["example1","example2", "example3"];
 const inputEl = document.getElementById("input-el");
 
 // This also works, but is pointless code in this case: const inputValue = document.getElementById("input-el").value;
@@ -8,4 +8,8 @@ inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     console.log(myLeads)
 })
+
+for (let i = 0; i < myLeads.length; i++){
+    console.log(myLeads[i]);
+}
 
