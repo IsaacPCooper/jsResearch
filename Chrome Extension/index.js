b18cd80531815219a334d6019aae3da88c5c7e7a
Isaac Philip Cooper <Jordan.P.C.Austin@gmail.com>
2022-05-05 18:52:37 +0100

    listItems, better performance in app

--- Chrome Extension/index.js
+++ Chrome Extension/index.js
@@ -3,6 +3,7 @@ const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
 let myLeads = ["example1","example2", "example3"];
 
+
 // This also works, but is pointless code in this case: const inputValue = document.getElementById("input-el").value;
 
 inputBtn.addEventListener("click", function() {
@@ -10,7 +11,15 @@ inputBtn.addEventListener("click", function() {
     console.log(myLeads)
 })
 
+let listItems = "";
 for (let i = 0; i < myLeads.length; i++){
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+    listItems += "<li>" + myLeads[i] + "</li>";
+    //ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+
+    // This also works, but may be harder to understand!
+    // document.createElement("li")
+    // li.textContent = myLeads[i];
+    // ulEl.append(li);
 }
+ulEl.innerHTML = listItems;
 
