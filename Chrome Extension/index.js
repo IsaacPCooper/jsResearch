1a6d87e67f833cc1e8781cbd8f3f9ca76e66a19c
Isaac Philip Cooper <Jordan.P.C.Austin@gmail.com>
2022-05-05 18:02:51 +0100

    addevent listener 1

--- Chrome Extension/index.js
+++ Chrome Extension/index.js
@@ -1,3 +1,9 @@
 function saveText() {
     console.log("Button Clicked!")
 }
+
+let inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function() {
+    console.log("addEventListener function")
+})
